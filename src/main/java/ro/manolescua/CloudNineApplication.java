63213166039c2cfdb9f819801ed5f9da20471b95
manolescua <manolescua@gmail.com>
2017-03-13 20:32:05 +0200

    [MAN-1] - Initial commit

---
+++ src/main/java/ro/manolescua/CloudNineApplication.java
@@ -0,0 +1,12 @@
+package ro.manolescua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNineApplication.class, args);
+	}
+}
